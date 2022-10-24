5aec8eae935b1737120dcab299e2977880054ff2
alexis lefoul <alex.lefoul@orange.fr>
2022-10-25 00:06:04 +0200

    init-2

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import logo from "./assets/appReact.png";
+import logo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
-
   return (
-    <div className="app">
-      <div>
-        <img src={logo} className="logo" alt="logo" />
+    <>
+      <div className="header">
+        <div className="c-logo">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
       </div>
-      <h1>ComResto</h1>
-    </div>
+      <div className="main"></div>
+    </>
   );
 }
 
